3150a85705f545198032ecc0390cb51937b9bb95
andrews <vavr.snf24@uea.edu.br>
2025-04-18 15:53:02 -0400

    added Media ponderada e aproveitamento

--- OneDrive/Área de Trabalho/AP1/src/PerformanceReport.java
+++ OneDrive/Área de Trabalho/AP1/src/PerformanceReport.java
@@ -1,11 +1,10 @@
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 
 public class PerformanceReport {
     private double mediaPonderada;
     private ArrayList<Double> notas = new ArrayList<>();
     private double aproveitamento;
-    private Student aluno;
     private ArrayList<Assessment> assessments = new ArrayList<>();
 
     //Notas individuais;
@@ -13,13 +12,65 @@ public class PerformanceReport {
         return notas;
     }
 
-    public void setNotasIndividuais(double nota) {
+    public void setNotasIndividuais(double nota,Assessment assessment) {
         notas.add(nota);
+        assessments.add(assessment);
 
     }
+
+    public void addAssessment(Assessment assessment) {
+        assessments.add(assessment);
+    }
+
+    public void removeAssessment(Assessment assessment) {
+        assessments.remove(assessment);
+    }
+
     public void removeNotasIndividuais(double nota){
         notas.remove(nota);
     }
-
     // Média ponderada;
+    public void setMediaPonderada() {
+        double acc = 0;
+        double accPeso = 0;
+        int i = 0;
+        for (double nota : notas) {
+            acc += nota *(assessments.get(i).getPeso());
+            accPeso += assessments.get(i).getPeso();
+
+
+        }
+        try{
+            this.mediaPonderada = acc / accPeso;
+
+        }catch (ArithmeticException e) {
+            System.out.println(e.getMessage() + " Algo de errado não está certo!");
+        }
+
+
+    }
+
+    public double getMediaPonderada() {
+        return mediaPonderada;
+    }
+    //Aproveitamento;
+    public void setAproveitamento() {
+        double acc = 0;
+        double accPeso = 0;
+        for (Assessment assessment : assessments) {
+            acc += assessment.getPeso()*assessment.getNotaMaxima();
+            accPeso += assessment.getPeso();
+
+        }
+        try{
+            double aproveitamentoMaximo = acc/accPeso;
+            this.aproveitamento = this.mediaPonderada/aproveitamentoMaximo;
+        }catch (ArithmeticException e) {
+            System.out.println(e.getMessage() + " Opa!");
+        }
+
+    }
+    public double getAproveitamento() {
+        return this.aproveitamento;
+    }
 }
